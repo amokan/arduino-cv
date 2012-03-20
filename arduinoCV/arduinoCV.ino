f02ed5534b538cb6a7847b31842f3653c2667aaa
Adam Mokan <adam.mokan@five9.com>
2012-03-19 18:35:15 -0700

    added simple PWM test code

--- arduinoCV/arduinoCV.ino
+++ arduinoCV/arduinoCV.ino
@@ -1,1 +1,20 @@
+// change the pwm duty cycle as needed (0-255)
+int pwm = 127;
 
+void setup()
+{
+  // set pin 11 to low-impedance state
+  pinMode(11, OUTPUT);
+  
+  // set freq to 61.5 khz
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS20);
+  
+  // duty cycle defined at the top of code
+  OCR2A = pwm;
+}
+
+void loop()
+{
+  OCR2A = pwm;  
+}
